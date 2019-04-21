7bac236f5a6c209c2edcf906cadc07ea58f85b9f
WillSkelton <william.f.skelton@gmail.com>
2019-04-20 20:22:31 -0700

    Background pt 2: electric boogaloo

--- game/game/Background.cpp
+++ game/game/Background.cpp
@@ -10,14 +10,23 @@ Background::Background(){
 		this->sprite1.setTexture(texture);
 		this->sprite2.setTexture(texture);
 
+		this->sprite1.scale(0.74, 0.74);
+		this->sprite2.scale(0.74, 0.74);
+
+		this->sprite1.setPosition(sf::Vector2f(0, 0));
+		this->sprite2.setPosition(sf::Vector2f(800, 0));
+
 	}
 }
 
-//DTOR
-Background::~Background(){}
+// DTOR
+Background::~Background(){
+	
+}
 
 void Background::showBackground(sf::RenderWindow &window){
 		
 	window.draw(this->sprite1);
+	window.draw(this->sprite2);
 
 }
